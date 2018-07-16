60392b6fdb91e33b5b67dd45aa0ce117aec4723a
Crimix <christofferm@live.dk>
2018-07-17 00:18:16 +0200

    - Removed support for baubles because of many weired stuff using baubles in belt  - Added own event that are fired on every itemstack in the belt

--- src/main/java/com/black_dog20/gadgetron/api/ISpecialEquipment.java
+++ src/main/java/com/black_dog20/gadgetron/api/ISpecialEquipment.java
@@ -1,12 +1,19 @@
 package com.black_dog20.gadgetron.api;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 
 public interface ISpecialEquipment {
 
 	SpecialEquipmentType getype();
 
 	public enum SpecialEquipmentType{
+		GENERAL_ITEM(0),
 		MAGNET(1),
 		MED(2);
 		
@@ -21,5 +28,24 @@ public interface ISpecialEquipment {
 	    }
 	}
 	
-	void onWornTick(EntityPlayer player);
+	public default void onWornTick(ItemStack stack, EntityPlayer player) {}
+	
+	public default void onTossEvent(EntityItem tossed, ItemStack calledOn, EntityPlayer player) {}
+	
+	public default void onPickUpEvent(EntityPlayer player, EntityItem item) {}
+	
+	public default void onAttackEvent(EntityLivingBase entity, DamageSource source, float amount) {}
+	
+	public default void onDeathEvent(EntityLivingBase entity, DamageSource source) {}
+	
+	public default void onHealEvent(EntityLivingBase entity, float amount) {}
+	
+	public default void onHurtEvent(EntityLivingBase entity, DamageSource source, float amount) {}
+	
+	public default void onFallEvent(EntityLivingBase entity, float distance, float damageMultiplier) {}
+	
+	public default void onFallFlyEvent(EntityPlayer player, float distance, float multiplier) {}
+	
+	public default void onStruckByLightningevent(Entity entity, EntityLightningBolt lightning) {}
+	
 }
--- src/main/java/com/black_dog20/gadgetron/handler/ItemEventHandler.java
+++ src/main/java/com/black_dog20/gadgetron/handler/ItemEventHandler.java
@@ -5,12 +5,20 @@ import com.black_dog20.gadgetron.capability.BeltHandler;
 import com.black_dog20.gadgetron.capability.IBeltHandler;
 import com.black_dog20.gadgetron.storage.CustomItemHandlerBase;
 
-import baubles.api.IBauble;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.living.LivingHealEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.entity.player.PlayerFlyableFallEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemEventHandler {
@@ -24,16 +32,205 @@ public class ItemEventHandler {
 				CustomItemHandlerBase iventory = bh.getInventory();
 				NonNullList<ItemStack> temp =  iventory.getStacks();
 				for(ItemStack stack : temp) {
-					if(stack.getItem() instanceof ISpecialEquipment) {
+					if(stack != null && !stack.isEmpty()) {
 						Item item = stack.getItem();
-						((ISpecialEquipment)item).onWornTick(player);
-					}
-					if(stack.getItem() instanceof IBauble) {
-						Item item = stack.getItem();
-						((IBauble)item).onWornTick(stack, player);
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onWornTick(stack,player);
+						}
 					}
 				}
 			}
 		}
 	}
+	
+	@SubscribeEvent
+	public void onItemToss(ItemTossEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onTossEvent(event.getEntityItem(), stack, player);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onEntityPickUpEvent(EntityItemPickupEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onPickUpEvent(player, event.getItem());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onAttack(LivingAttackEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onAttackEvent(event.getEntityLiving(), event.getSource(), event.getAmount());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onDeath(LivingDeathEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onDeathEvent(event.getEntityLiving(), event.getSource());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onHeal(LivingHealEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onHealEvent(event.getEntityLiving(), event.getAmount());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onHurt(LivingHurtEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onHurtEvent(event.getEntityLiving(), event.getSource(), event.getAmount());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onFall(LivingFallEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onFallEvent(event.getEntityLiving(), event.getDistance(), event.getDamageMultiplier());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onFallFly(PlayerFlyableFallEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onFallFlyEvent(event.getEntityPlayer(), event.getDistance(), event.getMultiplier());
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onStruckByLighting(EntityStruckByLightningEvent event){
+		if(event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			IBeltHandler bh = BeltHandler.instanceFor(player);
+			if(bh != null) {
+				CustomItemHandlerBase iventory = bh.getInventory();
+				NonNullList<ItemStack> temp =  iventory.getStacks();
+				for(ItemStack stack : temp) {
+					if(stack != null && !stack.isEmpty()) {
+						Item item = stack.getItem();
+						if(stack.getItem() instanceof ISpecialEquipment) {
+
+							((ISpecialEquipment)item).onStruckByLightningevent(event.getEntity(), event.getLightning());
+						}
+					}
+				}
+			}
+		}
+	}
+	
 }
